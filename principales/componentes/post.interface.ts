040f6e99fd56895aad81362beb9820f87c8a4c47
alberto96A <albertofassbender96@gmail.com>
2023-03-23 23:07:45 +0100

    interfaz post creada

---
+++ principales/componentes/post.interface.ts
@@ -0,0 +1,10 @@
+import { Post } from './post.service';
+
+const miPublicacion: Post = {
+    titulo: 'Mi Primerito Blog de Angular Segundo Intento',
+    texto: '¡Hola persona o concepto que lee esto!',
+    autor: 'Yo',
+    imagen: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F298856125249158329%2F&psig=AOvVaw1wdONFOL025lTLt4orC3OS&ust=1679695303712000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCMDsh-GG8_0CFQAAAAAdAAAAABAE',
+    fecha: new Date(),
+    categoria: 'General',
+};
